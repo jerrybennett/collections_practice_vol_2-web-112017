cc44e8defd5b5c688cf9fb6de1fdfa7dc44fcd7b
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-13 00:37:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,11 +26,7 @@ def merge_data(keys, data)
   # found in a stackoverflow thread
   new_arr = []
   # data[0].values.map.with_index {|v, i| keys[i].merge(v)}
-  data.each do |name, info|
-    info.each do |k, v|
-      new_arr << k
-    end
-  end
+
 end
 
 def find_cool(array)
